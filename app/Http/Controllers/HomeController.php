a1c1b691f1538930070c35464cb8e56078c9ed63
ismat <ismat.babirli@knexel.com>
2017-11-08 23:15:02 +0400

    header and footer ready

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ismat
+ * Date: 11/7/2017
+ * Time: 21:55
+ */
+
+namespace App\Http\Controllers;
+
+
+class HomeController {
+
+
+
+	public function Index(){
+
+		return view('index');
+	}
+}
---
+++ resources/views/footer.blade.php
@@ -0,0 +1,132 @@
+
+<style>
+
+</style>
+<section class="footer-info " style="margin-top: 80% ">
+
+    <div class="footer-links">
+        <div class="container">
+            <div class="row footer-top">
+                <div class="col-sm-3 footer-divs about hidden-xs">
+                    <h3>About Instant Access Health</h3>
+                    <div class="underline"></div>
+                    <p class="margin-top-10px">
+                        Instant - The Healthcare App is a consumer experience platform especially for health and wellness decision.Our platform gives people flexibility to get information about health care providers and gives people greater control and choice to connect with them. The extensive medical directory has access to over top doctors at over top hospitals & clinics. Instant portal uses technology driven app to empower healthcare consumers by giving them the ability to gather better, more timely information and advice about medical procedures, treatments.Be it a health insurance policy or pathology test we will help you to compare and gives people great control and choice to get the best at economical prices.                    </p>
+                    <ul class="follow-us">
+                        <li class="rb-social-icons">
+                            <a target="_blank" href="https://www.facebook.com/instantaccesshealthapp" class="rb-facebook" title="Instant Access Health on Facebook"><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li class="rb-social-icons">
+                            <a target="_blank" href="https://twitter.com/instantaccessht" class="rb-twitter" title="Instant Access Health on Twitter"><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li class="rb-social-icons">
+                            <a target="_blank" href="https://www.linkedin.com/company/instant-access-health" class="rb-linkedin" title="Instant Access Health on Linkedin"><i class="fa fa-linkedin"></i></a>
+                        </li>
+                    </ul>
+                    <!-- <div class="social-actions linkedinFollow">
+                        <script src="//platform.linkedin.com/in.js" type="text/javascript"> lang: en_US</script><script type="IN/FollowCompany" data-id="10332213" data-counter="top"></script>
+                    </div> -->
+                    <!-- <div class="social-actions facebookLike">
+                        <div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="box_count" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>
+                    </div> -->
+                </div>
+                <div class="col-sm-9 col-md-9 footer-divs padding0">
+                    <h3 class="hidden-xs">Quick Links</h3>
+                    <div class="underline hidden-xs"></div>
+                    <div class="row sub-head">
+                        <div class="col-sm-3 padding0">
+                            <h5 class="col-xs-12">Company</h5>
+                            <ul class="links-list">
+                                <li><a href="/about-us" title="About Us">About Us</a></li>
+                                <!-- <li><a href="/blog" target="blank">BA Zone</a></li> -->
+                                <li><a href="/pressandmedia" title="Press">Press</a></li>
+                                <li><a href="/sme" title="SME">SME</a></li>
+                                <!-- href="/two-wheeler-and-bike-loans" -->
+                            </ul>
+                        </div>
+                        {{--<div class="col-sm-3 padding0">--}}
+                            {{--<h5 class="col-xs-12">For Business</h5>--}}
+                            {{--<ul class="links-list">--}}
+                                {{--<li><a class="footerLoanLinks" href="/business-loan-india" title="Business Loan">Business Loan</a></li>--}}
+                                {{--<li><a class="footerLoanLinks" href="/mortgage-loan-against-property" title="Loan Against Property">Loan Against Property</a></li>--}}
+                                {{--<li><a class="" href="/commercial-vehicle-loan-and-finance" title="Commercial Vehicle">Commercial Vehicle</a></li>--}}
+                                {{--<li><a class="" href="/equipment-finance" title="Equipment Finance">Equipment Finance</a></li>--}}
+
+                                {{--<li><a class="" href="/education-loan" title="Education Loan">Education Loan</a></li>--}}
+                                {{--<li><a class="" href="/used-car-loans-and-vehicle-finance" title="Education Loan">Used Car Loan</a></li>--}}
+                                {{--<!-- href="/business-loan-india" -->--}}
+                                {{--<!-- href="/mortgage-loan-against-property" -->--}}
+                            {{--</ul>--}}
+                        {{--</div>--}}
+                        <div class="col-sm-3 col-xs-12 padding0">
+                            <h5 class="col-xs-12">Company</h5>
+                            <ul class="links-list">
+
+                                <li><a href="/career" title="Career">Career</a></li>
+                                <li><a href="/blog" target="blank" title="Blog">Knowledge Center</a></li>
+                                <li><a href="/contact" title="Contact Us">Contact Us</a></li>
+                            </ul>
+
+                        </div>
+                            {{--<div class="col-sm-3 col-xs-12 padding0">--}}
+
+                            {{--<h5 class="col-xs-12">Others</h5>--}}
+                            {{--<ul class="links-list">--}}
+
+                            {{--<li><a href="/tax" title="Tax">Tax</a></li>--}}
+                            {{--<li><a href="/gst" title="GST">GST</a></li>--}}
+                            {{--<li><a href="/mutual-fund" title="Mutual Fund">Mutual Fund</a></li>--}}
+                            {{--<!-- <li><a href="/financial-tools-calculators" title="Calculators">Calculators</a></li> -->--}}
+                            {{--<li><a href="/ifsc-code" title="IFSC Code">IFSC Code</a></li>--}}
+                            {{--<li><a href="/sitemap.html" title="Sitemap">Sitemap</a></li>--}}
+                            {{--<li><a href="/faq" title="FAQ">FAQ</a></li>--}}
+                            {{--</ul>--}}
+                            {{--</div>--}}
+
+
+                        {{--<div class="col-sm-3 col-xs-12 padding0">--}}
+
+                            {{--<h5 class="col-xs-12">Others</h5>--}}
+                            {{--<ul class="links-list">--}}
+
+                                {{--<li><a href="/tax" title="Tax">Tax</a></li>--}}
+                                {{--<li><a href="/gst" title="GST">GST</a></li>--}}
+                                {{--<li><a href="/mutual-fund" title="Mutual Fund">Mutual Fund</a></li>--}}
+                                {{--<!-- <li><a href="/financial-tools-calculators" title="Calculators">Calculators</a></li> -->--}}
+                                {{--<li><a href="/ifsc-code" title="IFSC Code">IFSC Code</a></li>--}}
+                                {{--<li><a href="/sitemap.html" title="Sitemap">Sitemap</a></li>--}}
+                                {{--<li><a href="/faq" title="FAQ">FAQ</a></li>--}}
+                            {{--</ul>--}}
+                        {{--</div>--}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright">
+        <div class="container">
+            <div class="row footer-bottom">
+
+                <div class="col-sm-5 col-md-4">
+                    <div class="download-app">
+                        <h5 class="download-app-text">Download App</h5>
+                        <span>
+												<a href="https://play.google.com/store/apps/details?id=in.instantaccess.instant" target="blank" title="Android App Link"><img src="/images/android-store.svg" alt="Download App"></a>
+											</span>
+                        <span>
+												<a style="padding-left: 0.5rem" href="http://instantaccesshealth.com/index.php" target="blank" title="Iphone App Link"><img src="/images/app-store.svg" alt="Download App"></a>
+											</span>
+                    </div>
+                    <p style="padding-top:5px !important;"><a href="/disclaimer" title="Disclaimer">Disclaimer</a> | <a href="/terms-of-use" title="Term of Use">Term of Use</a> | <a href="/privacy-policy" title="Privacy Policy">Privacy Policy</a></p>
+                </div>
+                <div class="col-sm-7 col-md-8 padding0">
+                    <p>Instant Access Health uses a combination of Secure Socket Layer (SSL) protocol and passwords to protect your information.</p>
+                    <img class="rb-norton" src="images/norton.png"><br>
+                    <p>	Copyright © 2017 IB Company</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer wrapper ends -->
+    <!-- Footer ends -->
+</section>
